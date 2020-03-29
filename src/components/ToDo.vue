a3e6a6ff64b2ad20fea5a2f086d1eab61ffa6b2d
pce260 <party4ur260.flugel@gmail.com>
2020-03-30 04:03:07 +0900

    ToDoを増やしたり減らしたりする
    ・編集機能なし
    ・ステータスによる表示制御なし
    ・ソートなし
    ・ローカルストレージによる永続化なし

---
+++ src/components/ToDo.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <h1>ToDo Application</h1>
+    <div>
+      <input type="text" v-model="newContent" @keyup.enter="addToDo()" placeholder="Input ToDo" />
+      <button @click="addToDo">Add</button>
+    </div>
+    <ul v-for="todo in todos" :key="todo.id">
+      <li>
+        <div>
+          <label>
+            <input type="checkbox" />{{todo.content}}
+          </label>
+          <button @click="deleteToDo(todo.id)">Delete</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  data()  {
+    return {
+      todos: [],
+      newContent: '',
+      currentId: 0
+    }
+  },
+  methods: { 
+    addToDo() {
+      if (this.newContent) {
+        this.todos.push({
+          id: this.currentId,
+          content: this.newContent,
+          status: 'in progress'
+        });
+        this.newContent = '';
+        this.currentId++;
+      }
+    },
+    deleteToDo(id) {
+      this.todos = this.todos.filter(function (todo) {
+        return todo.id !== id;
+      });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
