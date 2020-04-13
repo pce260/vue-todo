2ddd8fb64cb32633c7af8858516eba7a691525aa
pce260 <party4ur260.flugel@gmail.com>
2020-04-13 22:49:47 +0900

    機能改善
    ・編集機能を追加
    ・チェックボックスチェック時に「完了」表示を追加
    ・ローカルストレージによる永続化を実装
    ソートは未実装

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -2,16 +2,16 @@
   <div class="hello">
     <h1>ToDo Application</h1>
     <div>
-      <input type="text" v-model="newContent" @keyup.enter="addToDo()" placeholder="Input ToDo" />
+      <input type="text" v-model="newContent" @keyup.enter="addToDo()" placeholder="Input ToDo">
       <button @click="addToDo">Add</button>
     </div>
     <ul v-for="todo in todos" :key="todo.id">
       <li>
         <div>
-          <label>
-            <input type="checkbox" />{{todo.content}}
-          </label>
+          <input type="checkbox" v-model="todo.status" @change="updateToDo()">
+          <input type="text" v-model="todo.content" @change="updateToDo()">
           <button @click="deleteToDo(todo.id)">Delete</button>
+          <span v-show="todo.status">完了</span>
         </div>
       </li>
     </ul>
@@ -19,31 +19,52 @@
 </template>
 
 <script>
+const STORAGE_KEY = 'todos';
 export default {
   name: 'ToDo',
   data()  {
     return {
-      todos: [],
-      newContent: '',
-      currentId: 0
+      todos: JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]'),
+      newContent: ''
     }
   },
-  methods: { 
+  methods: {
     addToDo() {
       if (this.newContent) {
         this.todos.push({
-          id: this.currentId,
+          id: this.generateUuid(),
           content: this.newContent,
-          status: 'in progress'
+          status: false
         });
+        this.updateLocalStorage();
         this.newContent = '';
-        this.currentId++;
       }
     },
+    updateToDo() {
+      this.updateLocalStorage();
+    },
     deleteToDo(id) {
-      this.todos = this.todos.filter(function (todo) {
-        return todo.id !== id;
-      });
+      this.todos = this.todos.filter(todo => todo.id !== id);
+      this.updateLocalStorage();
+    },
+    updateLocalStorage() {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos));
+    },
+    generateUuid() {
+      // https://github.com/GoogleChrome/chrome-platform-analytics/blob/master/src/internal/identifier.js
+      // const FORMAT: string = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
+      let chars = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".split("");
+      for (let i = 0, len = chars.length; i < len; i++) {
+        switch (chars[i]) {
+          case "x":
+            chars[i] = Math.floor(Math.random() * 16).toString(16);
+            break;
+          case "y":
+            chars[i] = (Math.floor(Math.random() * 4) + 8).toString(16);
+            break;
+        }
+      }
+      return chars.join("");
     }
   }
 }
